646c7162de8f9628eee4d2b1bdeb74ae4fa701d9
Isaac Duff <4mustangid@gmail.com>
2023-03-23 09:21:22 -0600

    doing jobs

--- server/models/Job.js
+++ server/models/Job.js
@@ -9,25 +9,3 @@ export const JobsSchema = new Schema(
         description: { type: String, required: true, minLength: 3, maxLength: 500 }
     }
 )
-
-
-
-
-
-
-// import mongoose from "mongoose";
-// const Schema = mongoose.Schema
-
-// export const HousesSchema = new Schema(
-//     {
-//         bedrooms: { type: Number, required: true, min: 1, max: 10 },
-//         bathrooms: { type: Number, required: true, min: 1, max: 6 },
-//         levels: { type: Number, required: true, min: 1, max: 4 },
-//         year: { type: Number, required: true, min: 1800 },
-//         price: { type: Number, required: true, min: 100000, max: 1000000 },
-//         description: { type: String, required: true, maxLength: 500 },
-//         hasYard: { type: Boolean, default: true }
-//     },
-
-//     { timestamps: true }
-// )
